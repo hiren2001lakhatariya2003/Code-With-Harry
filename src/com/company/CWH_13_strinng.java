bd09c0db5a070d8bec001e8374e2496ed00f2592
Hiren Lakhatariya <200540107061@darshan.ac.in>
2022-08-15 10:37:48 +0530

    Create CWH_13_strinng.java

---
+++ src/com/company/CWH_13_strinng.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class CWH_13_strinng {
+    public static void main(String[] args) {
+        String a = "harry";
+        String a1 = new String("harry");
+
+    }
+}
