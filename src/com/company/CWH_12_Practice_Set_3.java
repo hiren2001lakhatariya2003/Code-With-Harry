9ba0883d63ceb9cada73554f4042479da1d94038
Hiren Lakhatariya <200540107061@darshan.ac.in>
2022-08-15 14:39:32 +0530

    Create CWH_12_Practice_Set_3.java

---
+++ src/com/company/CWH_12_Practice_Set_3.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class CWH_12_Practice_Set_3 {
+    public static void main(String[] args) {
+        //(1) Write a Java program to convert string to lowercase
+
+//        String s = new String("Hiren Lakhatariya");
+//        System.out.println(s.toLowerCase());
+
+        // (2) Write a Java program to replace space with _
+
+//        System.out.println(s.replace(" ","_"));
+
+        // (3) Write a Java program to fill in a letter template which Looks like a below:
+        // letter = "dear <|name|>, thanks a lot" , replace <|name|> with a string(some name)
+
+//        String name = "Dear <|name|> , Thanks a lot.";
+//        System.out.println(name.replace("<|name|>","hiren"));
+
+        //(4) Write a program to detect double and triple space in String.
+
+//        String str = "this is  my house , the Triranga   is waving on my  house   terraces";
+//        System.out.println(str.indexOf("  "));
+//        System.out.println(str.indexOf("   "));
+
+        //(5) Write a program to format the following later using escape sequence characters.
+//        String str = "this  \bis  my house ,\n the \'Triranga\'   is waving on my  \"house\"   terraces";
+//        System.out.println(str);
+    }
+}
