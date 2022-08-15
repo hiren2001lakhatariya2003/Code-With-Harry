e8a3eaf7da045e1f4bbd5965ddd271ad63ce82e6
Hiren Lakhatariya <200540107061@darshan.ac.in>
2022-08-15 14:39:19 +0530

    Update CWH_13_strinng.java

--- src/com/company/CWH_13_strinng.java
+++ src/com/company/CWH_13_strinng.java
@@ -1,9 +1,39 @@
 package com.company;
 
+import java.util.Locale;
+
 public class CWH_13_strinng {
     public static void main(String[] args) {
-        String a = "harry";
+       /* String a = "                       harry                     ";
         String a1 = new String("harry");
+        System.out.println(a);
+        System.out.println(a1);
+
+        int value = a.length();
+        System.out.println(value);
+        a=a.toUpperCase();
+        System.out.println(a);
+
+        String nonTrimmedString = "          hiren          ";
+        System.out.println(nonTrimmedString.stripLeading());*/
+        String name = "hirenhiren";
+//        System.out.println(name.substring(3,5)); //
+//        System.out.println(name.replace("h","sangita"));
+//
+//        System.out.println(name.startsWith("hiren"));
+//        System.out.println(name.endsWith("en"));
+
+//        System.out.println(name.charAt(3));
+//        System.out.println(name.lastIndexOf("i"));
+//        System.out.println(name.indexOf("z",3));
+        String a = "hiren";
+        String b = "HiRen";
+
+        System.out.println(a.equalsIgnoreCase(b));
+
+        System.out.format("i\b am\t\"hiren\"\n lakhatariya");
+
 
     }
+
 }
